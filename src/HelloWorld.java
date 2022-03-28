8ca4143d3091d06bfacb0c7afc896c112c4ea964
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-28 15:08:02 -0500

    completed HelloWorld Java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,82 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+//    int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+//    String myString = "Codeup";
+//        System.out.println(myString);
+//
+//        myString = "Code up";
+//        System.out.println(myString);
+
+//        myString = 3.14159;  error
+
+//        long myNumber;
+//        System.out.println(myNumber); error
+
+//        myNumber = 3.14; error
+
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+//
+//        myNumber = 123;
+//        System.out.println(myNumber);
+
+        // 3.14 has a decimal and will not work
+
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        // output above is 5 then 6
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        // output above is 6 then 6
+
+//        String class = "string";  error
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+        // can't cast a string to an int
+//
+//        System.out.println(three);
+
+//        int three = (int) "three";
+        // can't covert a string to int
+
+//        int x = 4;
+//        x = x + 5;
+//
+//        System.out.println(x);
+
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        y *= x;
+//
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+//        x /= y;
+//        y -= x;
+//
+//        System.out.println(x);  // 5
+//        System.out.println(y); // -3
+
+
     }
 }
