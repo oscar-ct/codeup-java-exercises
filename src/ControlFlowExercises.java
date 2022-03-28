8d8c9a75deaa5d8f38d4985b7047cbc0a4a39d40
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-28 16:12:57 -0500

    refactored do while loops into for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -26,6 +26,22 @@ public class ControlFlowExercises {
 
 
 
+//        for (int i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 100; i >= -10; i = i -5) {
+//            System.out.println(i);
+//        }
+
+//        for (long i = 2; i < 1000000; i = i * i) {
+//            System.out.println(i);
+//        }
+
+
+
+
+
 
 
 
