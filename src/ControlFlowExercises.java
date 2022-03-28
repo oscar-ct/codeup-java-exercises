ab8bad5f8b87b0fb2e6a6fdd7d3565f7ea74a125
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-28 16:06:55 -0500

    completed do while java exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,33 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i++);
+//        }
+
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i = i + 2;
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i = i - 5;
+//        } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = i * i;
+//        } while (i < 1000000);
+
+
+
+
+
+
+    }
+}
