71a87332c02f607e87676ba8a355a8f01e4bd261
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-29 16:09:45 -0500

    completed StringExercise, successful output for 4 variables

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String variable = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(variable);
+
+        String variable2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(variable2);
+
+        String variable3 = "In windows, the main drive is usually C:\\";
+        System.out.println(variable3);
+
+        String variable4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(variable4);
+
+    }
+}
