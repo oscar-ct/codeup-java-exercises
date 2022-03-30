c18ec5e6af891c33a51c62432419acd3f68b3c92
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-30 13:22:10 -0500

    commented out code

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -77,23 +77,23 @@ public class ControlFlowExercises {
 
 
 
-        String answer;
-       do {
-           System.out.print("Please enter a number 1-10:");
-
-           int input = sc.nextInt();
-
-           System.out.println("Here is your table! \n");
-           System.out.println("number | squared | cubed");
-           System.out.println("------ | ------- | -----");
-           for (int i = 1; i <= input; i++) {
-               int squared = i * i;
-               int cubed = i * i * i;
-               System.out.println(i + "\t   | " + squared + "\t\t | " + cubed);
-           }
-           System.out.println("Do you wish to continue? y or n");
-            answer = sc.next();
-       } while (answer.equals("y"));
+//        String answer;
+//       do {
+//           System.out.print("Please enter a number 1-10:");
+//
+//           int input = sc.nextInt();
+//
+//           System.out.println("Here is your table! \n");
+//           System.out.println("number | squared | cubed");
+//           System.out.println("------ | ------- | -----");
+//           for (int i = 1; i <= input; i++) {
+//               int squared = i * i;
+//               int cubed = i * i * i;
+//               System.out.println(i + "\t   | " + squared + "\t\t | " + cubed);
+//           }
+//           System.out.println("Do you wish to continue? y or n");
+//            answer = sc.next();
+//       } while (answer.equals("y"));
 
 
 
@@ -133,28 +133,28 @@ public class ControlFlowExercises {
 //        }
 
 
-//        String answer;
-//        do {
-//            System.out.print("Enter your grade number from 0-100:");
-//
-//            int input = sc.nextInt();
-//
-//            if (input >= 88) {
-//                System.out.println("You received an A");
-//            } else if (input >= 80) {
-//                System.out.println("You received an B");
-//            } else if (input >= 67) {
-//                System.out.println("You received an C");
-//            } else if (input >= 60) {
-//                System.out.println("You received an D");
-//            } else {
-//                System.out.println("You received an F");
-//            }
-//
-//            System.out.println("Do you wish to continue? y or n");
-//            answer = sc.next();
-//
-//        } while (answer.equals("y"));
+        String answer;
+        do {
+            System.out.print("Enter your grade number from 0-100:");
+
+            int input = sc.nextInt();
+
+            if (input >= 88) {
+                System.out.println("You received an A");
+            } else if (input >= 80) {
+                System.out.println("You received an B");
+            } else if (input >= 67) {
+                System.out.println("You received an C");
+            } else if (input >= 60) {
+                System.out.println("You received an D");
+            } else {
+                System.out.println("You received an F");
+            }
+
+            System.out.println("Do you wish to continue? y or n");
+            answer = sc.next();
+
+        } while (answer.equals("y"));
 
 
     }
