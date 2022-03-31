d00f12d5ebfa7f7c9f39ed2f0d0cecd5f529e094
Oscar Castro <oscar.a.castro818@gmail.com>
2022-03-30 22:23:52 -0500

    completed highlow exercise

---
+++ src/HighLow.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HighLow {
+    public static void guessingGame() {
+        System.out.println("Let's play a guessing game. . .");
+        System.out.println("I'm thinking of a number between 1 and 10, try and guess my number: ");
+        Scanner sc = new Scanner(System.in);
+        int computerValue = (int) (Math.random() * 10 + 1);
+        int numberOfTries = 0;
+        while (true) {
+            numberOfTries++;
+            int guessedNum = sc.nextInt();
+
+            if (guessedNum < computerValue) {
+                System.out.println("HIGHER");
+            } else if (guessedNum > computerValue) {
+                System.out.println("LOWER");
+            } else {
+                System.out.println("GOOD GUESS, it took you " + numberOfTries + " tries.");
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        guessingGame();
+    }
+}
