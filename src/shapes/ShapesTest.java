32e67f27a62fac85311fa8b5a5dc1b22f01be492
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-05 16:06:49 -0500

    feat: refactored using new methods

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -3,14 +3,25 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 
-        Rectangle box1 = new Rectangle(5, 4);
-        System.out.println(box1.getPerimeter());
-        System.out.println(box1.getArea());
+    Measurable myShape = new Rectangle(5, 4);
+        System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getArea());
 
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getPerimeter());
-        System.out.println(box2.getArea());
+    Measurable myShape1 = new Square(5);
+        System.out.println(myShape1.getPerimeter());
+        System.out.println(myShape1.getArea());
+
+
+
+//        Rectangle box1 = new Rectangle(5, 4);
+//        System.out.println(box1.getPerimeter());
+//        System.out.println(box1.getArea());
+//
+//
+//        Rectangle box2 = new Square(5);
+//        System.out.println(box2.getPerimeter());
+//        System.out.println(box2.getArea());
 
 
     }
