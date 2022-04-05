a6ebb08b01b9d7c3ba4d1d82fce5cc33da057165
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-05 16:04:59 -0500

    feat: refactored class to implement extend Quadrilateral class with overrides

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,21 +1,47 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable {
 
-     protected int length;
-     protected int width;
+//    protected int length;
+//    protected int width;
 
-    public Rectangle(int length, int width) {
+    public Rectangle(double length, double width) {
+        super(length, width);
+
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * length + 2 * width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    void setLength(double length) {
         this.length = length;
+    }
+
+    @Override
+    void setWidth(double width) {
         this.width = width;
     }
 
-    public int getArea() {
-        return this.length * this.width;
-    }
 
-    public int getPerimeter() {
-        return 2 * this.length + 2 * this.width;
-    }
+//     protected int length;
+//     protected int width;
+//    public Rectangle(int length, int width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//    public int getArea() {
+//        return this.length * this.width;
+//    }
+//    public int getPerimeter() {
+//        return 2 * this.length + 2 * this.width;
+//    }
 
 }
