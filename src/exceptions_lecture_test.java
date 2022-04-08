48cb88d36378a04c8a75ffeb728503a95cf04a74
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-07 22:24:00 -0500

    feat: created exceptions lecture test with main to run test code

---
+++ src/exceptions_lecture_test.java
@@ -0,0 +1,35 @@
+public class exceptions_lecture_test {
+    public static void main(String[] args) {
+
+
+
+        exceptions_lecture study = new exceptions_lecture();
+        study.subListMaker(null);
+
+
+
+
+
+//        study.askForAnInteger();
+
+//        try {
+//            exceptions_lecture study = new exceptions_lecture();
+//            System.out.println(study.certainlyNotAnInteger);
+//            System.out.println(Integer.parseInt(study.maybeAnInteger));
+//            System.out.println(Integer.parseInt(study.certainlyNotAnInteger));
+//        } catch (NumberFormatException nfe) {
+//            System.out.println("Got an exception " + nfe);
+//        }
+
+
+//        try {
+//            String str = "quick brown fox jumps over the lazy dog";
+//            System.out.println(str.length());
+//            System.out.println(str.substring(45));
+//        } catch (IndexOutOfBoundsException nfe){
+//            System.out.println("Hi");
+//        }
+
+
+    }
+}
