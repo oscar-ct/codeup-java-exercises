f5885e9dae29abee6b8c54beef4019fdbbd78589
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-11 11:57:18 -0500

    feat: refactored method use conditional logic to check for blank space

---
+++ src/ContactsManagerApp/ContactManagerTest.java
@@ -0,0 +1,12 @@
+package ContactsManagerApp;
+
+public class ContactManagerTest {
+    public static void main(String[] args) {
+
+        ContactsManager contacts = new ContactsManager();
+        contacts.mainMenu();
+        contacts.runApplication();
+
+    }
+
+}
