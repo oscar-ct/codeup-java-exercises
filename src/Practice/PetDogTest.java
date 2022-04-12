43add4978b642f218e8566150f552fcbdf3fba2f
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-11 21:59:38 -0500

    feat: created new array list testing all snuggle method

--- src/Practice/PetDogTest.java
+++ src/Practice/PetDogTest.java
@@ -1,12 +1,25 @@
 package Practice;
 
+import java.util.ArrayList;
+
 public class PetDogTest {
     public static void main(String[] args) {
 
 
-    PetDog petdog = new PetDog("hello", "sup");
+    PetDog petdog = new PetDog("Spot", "Mut", true);
+    PetDog petdog1 = new PetDog("Spark", "Mut", false);
+    PetDog petdog2 = new PetDog("Blue", "Mut", true);
 
-    petdog.snuggle();
+        System.out.println(petdog.snuggle());
+        System.out.println(petdog.isTrained());
+
+        ArrayList<PetDog> pets = new ArrayList<>();
+        pets.add(petdog);
+        pets.add(petdog1);
+        pets.add(petdog2);
+        PetDog.allSnuggle(pets);
+
+
 
     }
 }
