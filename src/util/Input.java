2f78d8b95171f67a0f906dbcd46f57e385f6c604
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-11 22:02:52 -0500

    feat: created new method to capitalize the first letter of a word

--- src/util/Input.java
+++ src/util/Input.java
@@ -6,8 +6,26 @@ public class Input {
     private Scanner sc = new Scanner(System.in);
 
     public String getString() {
-
         return sc.nextLine();
+    }
+
+    public String getStringCapitalized() {
+        String input = sc.nextLine();
+        return capitalizeString(input);
+    }
+
+    public String capitalizeString(String string) {
+        char[] chars = string.toLowerCase().toCharArray();
+        boolean found = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!found && Character.isLetter(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                found = true;
+            } else if (Character.isWhitespace(chars[i]) || chars[i]=='.' || chars[i]=='\'') { // You can add other chars here
+                found = false;
+            }
+        }
+        return String.valueOf(chars);
     }
 
     public boolean yesNo() {
