4f164abedc98b13321d55903bad1d0012f967781
Oscar Castro <oscar.a.castro818@gmail.com>
2022-04-28 23:59:26 -0500

    feat: created 2 quote and 2 authors instances with new data and added to arraylist

--- src/JavaBeans/BeanTest.java
+++ src/JavaBeans/BeanTest.java
@@ -15,16 +15,24 @@ public class BeanTest {
 
         Author authorOne = new Author(1, "Stephen", "King");
         Author authorTwo = new Author(2, "Mark", "Twain");
-
+        Author authorThree = new Author(2, "Albert", "Einstein");
+        Author authorFour = new Author(2, "Nikola", "Tesla");
 
         Quotes quoteOne = new Quotes(1, authorOne, "Books are a uniquely portable magic");
         Quotes quoteTwo = new Quotes(2, authorTwo, "Always obey your parents when they are present");
+        Quotes quoteThree = new Quotes(3, authorThree, "We cannot solve our problems with the same thinking we used when we created them");
+        Quotes quoteFour = new Quotes(3, authorThree, "You want to find the secrets of the universe, think in terms of energy, frequency and vibration");
+
+
+
 
 
         ArrayList<Quotes> quoteArray = new ArrayList<>();
 
         quoteArray.add(quoteOne);
         quoteArray.add(quoteTwo);
+        quoteArray.add(quoteThree);
+        quoteArray.add(quoteFour);
 
         for (int i = 0; i < quoteArray.size(); i++) {
             System.out.print(quoteArray.get(i).getAuthor().getFirstName() + " ");
