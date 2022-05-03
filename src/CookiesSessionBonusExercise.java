67622661caf9ad6c6234f163780b8df434189dbc
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-02 22:51:40 -0500

    feat: completed sessions bonus exercise on seperate branch, added comments explaining exercise with link to java code

---
+++ src/CookiesSessionBonusExercise.java
@@ -0,0 +1,8 @@
+public class CookiesSessionBonusExercise {
+
+    // completed a challenging java session bonus exercise by Justin Reich.
+    // this bonus involved creating an empty arraylist/session-key upon user login and populating arraylist by selecting from the users existing ads jsp and displaying this on a new jsp file followed by a clear functionality via button.
+
+    //link:
+    // https://github.com/oscar-ct/java-adlister/tree/wishlist-bonus/src/main/java
+}
