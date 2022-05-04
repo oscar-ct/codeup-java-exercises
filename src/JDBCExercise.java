1d711299a3ba822f1b9fac0bad16a987df243aa7
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-03 23:56:35 -0500

    feat: completed JDBC exercise on seperate branch, added comments explaining exercise with link to java code

---
+++ src/JDBCExercise.java
@@ -0,0 +1,10 @@
+public class JDBCExercise {
+
+
+    // completed a challenging JDBC java exercise by Codeup curriculum.
+    // this exercise involved creating a java class that connects to a sql database and displays table data to the browser and also inserts data from the browser to the table.
+
+    //link:
+    // https://github.com/oscar-ct/java-adlister/tree/jdbc-exercise
+
+}
