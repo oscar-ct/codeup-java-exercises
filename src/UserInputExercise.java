9a70a215b42ddbc201f565778767fbe25f9a1d1b
Oscar Castro <oscar.a.castro818@gmail.com>
2022-05-05 22:09:47 -0500

    completed userinput exercise on seperate branch: refactored vulnerabilities in jdbc methods using prepared statements, created new user dao, added hyperlinks to navbar, created a form jsp allowing user to create a new accounts and more

---
+++ src/UserInputExercise.java
@@ -0,0 +1,12 @@
+public class UserInputExercise {
+
+    // completed a jdbc exercise/ user input using java, mysql and jsp/jstl
+    // "refactored vulnerabilities in jdbc methods using prepared statements, created new user dao, added hyperlinks to navbar, created a form jsp allowing user to create a new accounts and more"
+
+    //link:
+    // https://github.com/oscar-ct/java-adlister/tree/user-input-exercise
+}
+
+
+
+
